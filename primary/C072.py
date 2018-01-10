5de3dd71442bb775f8f4a53d3ccd27703fe7748b
DearZack <zack4work@gmail.com>
2018-01-10 23:10:41 +0800

    Python初体验

---
+++ primary/C072.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    ptr = []
+    for i in range(5):
+        num = int(input("数字:"))
+        ptr.append(num)
+    print(ptr)
---
+++ primary/C073.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    ptr = []
+    for i in range(5):
+        num = int(input("数字:"))
+        ptr.append(num)
+    ptr.reverse()
+    print(ptr)
